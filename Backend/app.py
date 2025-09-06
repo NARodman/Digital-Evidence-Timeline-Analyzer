ce913cf48c3250b64cd6ecaf065096db21bed4bf
NARodman <ninaalanier8@gmail.com>
2025-09-06 19:54:05 -0400

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,3 +1,13 @@
 from Flask import Flask, jsonify
 
 app = Flask(__name__)
+
+#test route
+
+@app.route('/test', methods=['GET'])
+def hello():
+    return jsonify({"message": "Backend is alive!"})
+
+
+if __name__ == "__main__": 
+    app.run(debug=True, port=5000)
